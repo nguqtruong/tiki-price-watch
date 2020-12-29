a5c3a1df9ca0dfd4a00096829b189781074f950e
Ngu <zquangu112z@gmail.com>
2020-12-29 17:15:16 +0700

    check if id is NULL

--- scrape.sh
+++ scrape.sh
@@ -6,7 +6,10 @@ while IFS= read -r line; do
     a=`echo $line | grep -o "\-p\(\d\+\).html" | grep -o "\d\+"`
     # echo $a;
 
-    # Get current price
-    curl "https://tiki.vn/api/v2/products/$a/info?platform=web" | jq -r '.price' > output/$a.txt;
-    echo Link sản phẩm: $line >> output/$a.txt;
+    if [[ -n "$a" ]]; then  # check if a is NULL
+        # Get current price
+        curl "https://tiki.vn/api/v2/products/$a/info?platform=web" | jq -r '.price' > output/$a.txt;
+        echo Link sản phẩm: $line >> output/$a.txt;
+    fi
+
 done < links.txt
