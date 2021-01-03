034fbd1829bcf01cc02287f1e494163aa3484b54
Ngu Truong <zquangu112z@gmail.com>
2021-01-04 00:36:00 +0700

    test ci

--- scrape.sh
+++ scrape.sh
@@ -2,8 +2,11 @@
 
 # TIKI price
 echo hihi
-curl "https://tiki.vn/api/v2/products/4538233/info?platform=web" | jq -r '.price' > output/4538233.txt;
+# curl "https://tiki.vn/api/v2/products/4538233/info?platform=web" | jq -r '.price' > output/4538233.txt;
+cat links.txt;
+
 while IFS= read -r line; do
+    echo $line
     # Get product id
     a=`echo $line | grep -o "\-p\(\d\+\).html" | grep -o "\d\+"`
     b=`echo $line | grep -o "\.*spid\=\(\d\+\)" | grep -o "\d\+"`
